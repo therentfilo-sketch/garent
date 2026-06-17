392dca9df964ade2bf7e9f0dd68d5b37b26fa3d7
therentfilo-sketch <therentfilo@gmail.com>
2026-06-17 23:36:48 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SITE_CONFIG = {
   whatsappNumber: "905019558524",
   callNumber: "905019558524",
   email: "info@garentfilo.com",
-  city: " Türkiye",
+  city: "İstanbul, Türkiye",
   whatsappUrl(message = "") {
     const text = message ? `?text=${encodeURIComponent(message)}` : "";
     return `https://wa.me/${this.whatsappNumber}${text}`;
