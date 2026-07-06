07059b152711132183c1498cd94436c93b405130
therentfilo-sketch <therentfilo@gmail.com>
2026-07-07 01:04:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.SITE_CONFIG = {
   siteName: "GARENT FİLO",
   siteTitle: "GARENT FİLO | Premium Araç Kiralama",
-  whatsappNumber: "905543428004",
-  callNumber: "905532937374",
+  whatsappNumber: "905545367956",
+  callNumber: "905513722656",
   email: "info@garentfilo.com",
   city: "İstanbul, Türkiye",
   whatsappUrl(message = "") {
